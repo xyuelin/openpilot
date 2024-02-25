9ec1411adfcf42865699e585555d56feb574367d
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-25 19:19:43 +0000

    February 25, 2024 Update

--- selfdrive/car/volkswagen/pqcan.py
+++ selfdrive/car/volkswagen/pqcan.py
@@ -91,12 +91,12 @@ def create_acc_accel_control(packer, bus, acc_type, acc_enabled, accel, acc_cont
   return commands
 
 
-def create_acc_hud_control(packer, bus, acc_hud_status, set_speed, lead_distance):
+def create_acc_hud_control(packer, bus, acc_hud_status, set_speed, lead_distance, personality_profile):
   values = {
     "ACA_StaACC": acc_hud_status,
     "ACA_Zeitluecke": 2,
     "ACA_V_Wunsch": set_speed,
-    "ACA_gemZeitl": lead_distance,
+    "ACA_gemZeitl": 1 if personality_profile == 0 else 3 if personality_profile == 1 else 5,
     "ACA_PrioDisp": 3,
     # TODO: restore dynamic pop-to-foreground/highlight behavior with ACA_PrioDisp and ACA_AnzDisplay
     # TODO: ACA_kmh_mph handling probably needed to resolve rounding errors in displayed setpoint
