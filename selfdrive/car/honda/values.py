b7b9344614e12e920e8a0e9c894b9c747998d478
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-27 16:34:47 -0700

    Honda Clarity Port
    Co-Authored-By: mike8643 <98910897+mike8643@users.noreply.github.com>

--- selfdrive/car/honda/values.py
+++ selfdrive/car/honda/values.py
@@ -195,6 +195,12 @@ class CAR(Platforms):
     CarSpecs(mass=3338.8 * CV.LB_TO_KG, wheelbase=2.5, centerToFrontRatio=0.5, steerRatio=16.71, tireStiffnessFactor=0.82),
     dbc_dict('acura_rdx_2020_can_generated', None),
   )
+  CLARITY = HondaBoschPlatformConfig(
+    "HONDA CLARITY 2018",
+    [HondaCarDocs("Honda Clarity 2018-22", "All", min_steer_speed=3. * CV.MPH_TO_MS)],
+    CarSpecs(mass=4052. * CV.LB_TO_KG, wheelbase=2.75, centerToFrontRatio=0.41, steerRatio=16.50, tireStiffnessFactor=1.),
+    dbc_dict('honda_clarity_hybrid_2018_can_generated', 'acura_ilx_2016_nidec'),
+  )
 
   # Nidec Cars
   ACURA_ILX = HondaNidecPlatformConfig(
