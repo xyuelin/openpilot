e010c743d2dc2c7277367ba463908e6d1b9f1211
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-03-27 06:43:40 -0700

    Stylistic map UIs and MapBox styles
    Fancied up the map in the onroad UI and added toggles to use a custom MapBox style when using "Navigate on openpilot",
    
    Co-Authored-By: mike8643 <98910897+mike8643@users.noreply.github.com>
    Co-Authored-By: pencilpusher <83676301+jakethesnake420@users.noreply.github.com>
    Co-Authored-By: multikyd <72329880+multikyd@users.noreply.github.com>

--- common/params.cc
+++ common/params.cc
@@ -318,6 +318,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"MapboxSecretKey", PERSISTENT},
     {"MapsSelected", PERSISTENT},
     {"MapSpeedLimit", PERSISTENT},
+    {"MapStyle", PERSISTENT},
     {"MapTargetLatA", PERSISTENT},
     {"MapTargetVelocities", PERSISTENT},
     {"Model", PERSISTENT},
--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -262,6 +262,7 @@ typedef struct UIScene {
   int custom_signals;
   int desired_follow;
   int driver_camera_timer;
+  int map_style;
   int obstacle_distance;
   int obstacle_distance_stock;
   int screen_brightness;
